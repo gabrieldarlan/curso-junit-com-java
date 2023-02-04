325be151dbcd313607c05f9bb5626e192e8472ce
Gabriel Darlan <gabriel.darlan88@gmail.com>
2023-02-04 13:25:48 -0300

    criacao do UseResource

---
+++ src/main/java/br/com/gdarlan/api/resource/UserResource.java
@@ -0,0 +1,17 @@
+package br.com.gdarlan.api.resource;
+
+import br.com.gdarlan.api.domain.User;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/user")
+public class UserResource {
+    @GetMapping("/{id}")
+    public ResponseEntity<User> findById(@PathVariable Integer id){
+        return ResponseEntity.ok().body(new User(1,"maria","maria@email.com","123"));
+    }
+}
