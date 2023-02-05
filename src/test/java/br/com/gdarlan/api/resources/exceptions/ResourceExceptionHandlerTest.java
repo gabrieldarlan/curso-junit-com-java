3db1fc1f0bf4e90960752fb5655efb051f9047bb
Gabriel Darlan <gabriel.darlan88@gmail.com>
2023-02-05 14:09:36 -0300

    teste da classe ResourceExceptionHandler - metodo dataIntegrityViolation

--- src/test/java/br/com/gdarlan/api/resources/exceptions/ResourceExceptionHandlerTest.java
+++ src/test/java/br/com/gdarlan/api/resources/exceptions/ResourceExceptionHandlerTest.java
@@ -1,5 +1,6 @@
 package br.com.gdarlan.api.resources.exceptions;
 
+import br.com.gdarlan.api.services.exceptions.DataIntegrityViolationException;
 import br.com.gdarlan.api.services.exceptions.ObjectNotFoundException;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -10,6 +11,8 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.mock.web.MockHttpServletRequest;
 
+import java.time.LocalDateTime;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
@@ -18,6 +21,7 @@ class ResourceExceptionHandlerTest {
     public static final int INDEX = 0;
     public static final String E_MAIL_JA_CADASTRADO_NO_SISTEMA = "E-mail já cadastrado no sistema.";
     public static final int CODE_NOT_FOUND = 404;
+    public static final int CODE_BAD_REQUEST = 400;
     @InjectMocks
     private ResourceExceptionHandler exceptionHandler;
 
@@ -43,5 +47,19 @@ class ResourceExceptionHandlerTest {
 
     @Test
     void dataIntegrityViolation() {
+        ResponseEntity<StandardError> response =
+                exceptionHandler.dataIntegrityViolation(new DataIntegrityViolationException(E_MAIL_JA_CADASTRADO_NO_SISTEMA),
+                        new MockHttpServletRequest());
+
+        assertNotEquals("/user/2", response.getBody().getPath());
+        assertNotEquals(LocalDateTime.now(), response.getBody().getTimestamp());
+        assertNotNull(response);
+        assertNotNull(response.getBody());
+        assertEquals(HttpStatus.BAD_REQUEST, response.getStatusCode());
+        assertEquals(ResponseEntity.class, response.getClass());
+        assertEquals(StandardError.class, response.getBody().getClass());
+        assertEquals(E_MAIL_JA_CADASTRADO_NO_SISTEMA, response.getBody().getError());
+        assertEquals(CODE_BAD_REQUEST, response.getBody().getStatus());
+
     }
 }
