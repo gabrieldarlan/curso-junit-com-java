b44ee1299e5f5a86379e8bd207beeba43bcbc869
Gabriel Darlan <gabriel.darlan88@gmail.com>
2023-02-04 22:09:49 -0300

    teste para classe UserService metodo delete

--- src/test/java/br/com/gdarlan/api/services/impl/UserServiceImplTest.java
+++ src/test/java/br/com/gdarlan/api/services/impl/UserServiceImplTest.java
@@ -20,8 +20,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyString;
-import static org.mockito.Mockito.anyInt;
-import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.*;
 
 @SpringBootTest
 class UserServiceImplTest {
@@ -150,7 +149,22 @@ class UserServiceImplTest {
     }
 
     @Test
-    void delete() {
+    void deleteWithSuccess() {
+        when(repository.findById(anyInt())).thenReturn(optionalUsers);
+        doNothing().when(repository).deleteById(anyInt());
+        service.delete(ID);
+        verify(repository, times(1)).deleteById(anyInt());
+    }
+
+    @Test
+    void deleteWithNotFoundException() {
+        when(repository.findById(anyInt())).thenThrow(new ObjectNotFoundException(OBJETO_NAO_LOCALIZADO));
+        try {
+            service.delete(ID);
+        } catch (Exception ex) {
+            assertEquals(ObjectNotFoundException.class, ex.getClass());
+            assertEquals(OBJETO_NAO_LOCALIZADO, ex.getMessage());
+        }
     }
 
     private void startUser() {
